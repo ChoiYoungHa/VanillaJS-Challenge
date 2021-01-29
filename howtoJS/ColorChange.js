311dbd873a7ce529595274c5d8d54f1947ccb7ff
YoungHaChoi <askillofgod159@naver.com>
2021-01-29 18:11:58 +0900

    commit

--- howtoJS/ColorChange.js
+++ howtoJS/ColorChange.js
@@ -2,7 +2,7 @@ const title = document.querySelector("#title")
 const BASE_CORLOR = "rgb(27, 156, 252)";
 const ANOTHER_COLOR = "#55E6C1";
 
-function handlerClick(){
+function handlerClick(){ // 베이스색을 console.log로 찍어보고 rgb값 상수로 지정해두어야함.
     const currentColor = title.style.color;
     if(currentColor === BASE_CORLOR){
         title.style.color = ANOTHER_COLOR;
@@ -20,6 +20,5 @@ function okey(){
     console.log("Are you click??!");
 }
 init();
-
 
 window.addEventListener("click",okey);
