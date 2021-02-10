726ed454b712c08047b87e853871245b9b03a045
YoungHaChoi <askillofgod159@naver.com>
2021-02-10 21:15:55 +0900

    commit

--- howtoJS/weather.js
+++ howtoJS/weather.js
@@ -1,6 +1,20 @@
 const API_KEY = 'd3341bf154506acda418dfd7ce66eb7c';
 const COORDS = 'coords'
+const weather = document.querySelector(".js-weather");
 
+
+function getWeather(lat, lon){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
+    .then(function(response){
+        return response.json()
+    }).then(function(json){
+        console.log(json);
+        const temp = json.main.temp;
+        const location = json.name;
+        weather.innerText = `${temp}@${location}`;
+        
+    });
+}
 
 function saveCoorsObj(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -10,11 +24,12 @@ function handleGeoSuccess(position){
     const latitude = position.coords.latitude; // position 객체에서 위도
     const longitude = position.coords.longitude; // 경도 읽어오기
     const coordsObj = {
-        latitude : latitude, // 각 name값 value값이 같으면 그냥 하나만 적어도 된다고함.
-        longitude : longitude
+        latitude, // 각 name값 value값이 같으면 그냥 하나만 적어도 된다고함.
+        longitude
     };
 
     saveCoorsObj(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError(){
@@ -30,7 +45,8 @@ function loadCoords(){
     if(loadCoords === null){
         askforCoors();
     } else{
-        //getWeather
+        const parseCoords = JSON.parse(loadCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
